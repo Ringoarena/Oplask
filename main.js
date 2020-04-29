5a255f91da00ccfb2261b9d2a8fd6c75e67f6ce7
Rikard Virta <rikard.virta90@gmail.com>
2020-04-29 16:55:55 +0200

    working on download functionality and styling of lightbox

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const IMAGE_CONTAINER = document.querySelector(".imageContainer");
 const SUBMIT_BUTTON = document.querySelector(".submit");
 const SEARCH_BOX = document.querySelector(".search-box");
 const VIEW_FAVORITES_BUTTON = document.querySelector(".viewFavorites");
+const CLEAR_FAVORITES_BUTTON = document.querySelector(".clearFavorites");
 const PREVIOUS_BUTTON = document.querySelector(".previousButton");
 const NEXT_BUTTON = document.querySelector(".nextButton");
 const LIGHTBOX = document.querySelector(".lightbox");
@@ -10,7 +11,6 @@ const LIGHTBOX_IMAGE = document.querySelector(".lightbox-image");
 const LIGHTBOX_ADD_FAVORITE_BUTTON = document.querySelector(".add-favorite");
 const LIGHTBOX_DOWNLOAD_IMAGE_BUTTON = document.querySelector(".download-image");
 const LIGHTBOX_CLOSE_BUTTON = document.querySelector(".close-lightbox")
-
 
 // local data
 let currentImage;
@@ -54,6 +54,11 @@ function renderImages(container, images) {
   }
 }
 
+// download functions
+async function downloadImage(image) {
+  download(image.links.download);
+}
+
 // event callback functions
 function submitButtonClickHandler() {
   currentSearchValue = SEARCH_BOX.value;
@@ -62,7 +67,15 @@ function submitButtonClickHandler() {
 }
 
 function viewFavoritesButtonClickHandler() {
-  alert("view fav button clicked");
+  LIGHTBOX.classList.add("hidden");
+  renderImages(IMAGE_CONTAINER, favorites);
+}
+
+function clearFavoritesButtonClickHandler() {
+  localStorage.clear()
+  favorites = []
+  renderImages(IMAGE_CONTAINER, favorites)
+  alert("Clear favorites click handler called");
 }
 
 function previousButtonClickHandler() {
@@ -86,29 +99,39 @@ function imageClickHandler(image) {
 }
 
 function addFavoritesClickHandler() {
-  // alert("add favorites click handler called");
-
-  // Retrieve favorites
-  favorites = JSON.parse(localStorage("favorites"));
-  // Remove favorites from local storage
-  localStorage.removeItem("favorites")
+  if (localStorage.getItem('favorites') != null) {
+    // Retrieve favorites
+    favorites = JSON.parse(localStorage.getItem('favorites'));
+    // Remove favorites from local storage
+    localStorage.removeItem('favorites')
+  }
   // Modify favorites
-  favorites.push(image)
+  favorites.push(currentImage)
   // Save favorites
-  localStorage("favorites") = JSON.stringify(favorites);
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+  console.log(localStorage.getItem('favorites'))
 }
 
 function downloadImageClickHandler() {
-  alert("download image click handler called");
+  downloadImage(currentImage);
 }
 
 function closeLightBoxClickHandler() {
   LIGHTBOX.classList.toggle("hidden");
 }
 
+function loadDatabase() {
+  if (localStorage.getItem('favorites') != null) {
+
+    // Retrieve favorites
+    favorites = JSON.parse(localStorage.getItem('favorites'));
+  }
+}
+
 function initiateListeners() {
    SUBMIT_BUTTON.addEventListener("click", submitButtonClickHandler);
    VIEW_FAVORITES_BUTTON.addEventListener("click", viewFavoritesButtonClickHandler);
+   CLEAR_FAVORITES_BUTTON.addEventListener("click", clearFavoritesButtonClickHandler)
    PREVIOUS_BUTTON.addEventListener("click", previousButtonClickHandler);
    NEXT_BUTTON.addEventListener("click", nextButtonClickHandler);
    LIGHTBOX_ADD_FAVORITE_BUTTON.addEventListener("click", addFavoritesClickHandler);
@@ -117,6 +140,7 @@ function initiateListeners() {
 }
 
 function run() {
-  initiateListeners()
+  initiateListeners();
+  loadDatabase()
 }
 run();
